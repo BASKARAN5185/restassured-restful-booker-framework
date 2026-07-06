dde3f76074e2970fc974a5d53b98b65ffa3c525c
BASKARAN5185 <baskarbala5185@gmail.com>
2026-07-06 12:43:08 +0530

    Add Bookingid Pojo request and response

---
+++ RestAssuredFramework/src/test/java/payloads/BookingId.java
@@ -0,0 +1,18 @@
+package payloads;
+
+public class BookingId {
+    private int bookingId;
+
+    public void boookingId(){
+
+    }
+
+    public int getBookingId(){
+        return bookingId;
+    }
+
+    public void setBookingId( int bookingId){
+       this.bookingId=bookingId;
+    }
+    
+}
